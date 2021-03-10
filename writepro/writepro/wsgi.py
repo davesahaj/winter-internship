808924a4b76cf8f8f6ee7a8823bee1364c5dc435
dave <sahajdave@gmail.com>
2021-03-10 11:58:10 +0530

    added django react framework

--- writepro/writepro/wsgi.py
+++ writepro/writepro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for winter_internship project.
+WSGI config for writepro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'winter_internship.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'writepro.settings')
 
 application = get_wsgi_application()
